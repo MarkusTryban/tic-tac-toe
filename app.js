f0c71faa77b15afec989a3e10bfeb66daa6acf08
MarkusTryban <markustryban@gmail.com>
2020-03-14 16:28:24 -0400

    Update Square & Board methods

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ import React from 'react';
 import './App.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className='square'>{this.props.value}</button>;
+    return (
+      <button className='square' onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
